9e36fcf3822683892b5a9d7f2449d16a2eef7193
mahmood <mahmood8664@gmail.com>
2021-02-15 14:51:22 +0330

    enable ssl

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 export class Config {
     public static readonly baseIp = "api.battleship.mamiri.me";
     // public static readonly baseIp = "192.168.1.118:8080";
-    public static readonly restProtocol = "http://";
-    public static readonly socketProtocol = "ws://";
+    public static readonly restProtocol = "https://";
+    public static readonly socketProtocol = "wss://";
     public static readonly restUrl = Config.restProtocol + Config.baseIp + "/api/v1";
     public static readonly socketUrl = Config.socketProtocol + Config.baseIp + "/socket";
 }
